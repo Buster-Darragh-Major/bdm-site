b6bf9a75060cfcb22090f735651d2f2ec4afc534
Buster Darragh-Major <busterdarragh-major@Busters-Mac.Home>
2022-06-21 17:57:29 +0100

    fix text aligning stuff and change off white

--- src/components/FillShelves.js
+++ src/components/FillShelves.js
@@ -3,30 +3,24 @@ import TypingTitle from "./TypingTitle.js";
 function FillShelves(props) {
     return (
         <div>
-            <section className="center-child fill-view scroll-snap-child">
-                <TypingTitle titles={props.titles}
-                    titleHangTime="4000"
-                    emptyHangTime="1700" />
+            <section className="fill-view scroll-snap-child flex-center">
+                <div className="x-center-text padding-l">
+                    <TypingTitle titles={props.titles}
+                        titleHangTime="4000"
+                        emptyHangTime="1700" />
+                </div>
             </section>
-            <section className="center-child fill-view scroll-snap-child background-navy">
-                <TypingTitle titles={props.titles}
-                    titleHangTime="4000"
-                    emptyHangTime="1700" />
+            <section className="fill-view scroll-snap-child background-navy">
+                
             </section>
-            <section className="center-child fill-view scroll-snap-child">
-                <TypingTitle titles={props.titles}
-                    titleHangTime="4000"
-                    emptyHangTime="1700" />
+            <section className="fill-view scroll-snap-child">
+                
             </section>
-            <section className="center-child fill-view scroll-snap-child background-green">
-                <TypingTitle titles={props.titles}
-                    titleHangTime="4000"
-                    emptyHangTime="1700" />
+            <section className="fill-view scroll-snap-child background-green">
+                
             </section>
-            <section className="center-child fill-view scroll-snap-child">
-                <TypingTitle titles={props.titles}
-                    titleHangTime="4000"
-                    emptyHangTime="1700" />
+            <section className="fill-view scroll-snap-child">
+                
             </section>
         </div>
     );
