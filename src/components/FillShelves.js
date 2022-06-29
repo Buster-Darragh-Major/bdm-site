4d69c43f1512d733fd229cb90e2a5204dcccb41c
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2022-06-30 00:56:20 +0100

    add scroll arrow

--- src/components/FillShelves.js
+++ src/components/FillShelves.js
@@ -1,27 +1,35 @@
 import TypingTitle from "./TypingTitle.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
 
 function FillShelves(props) {
     return (
         <div>
-            <section className="fill-view scroll-snap-child flex-center">
-                <div className="x-center-text padding-l">
-                    <TypingTitle titles={props.titles}
-                        titleHangTime="4000"
-                        emptyHangTime="1700" />
-                </div>
-            </section>
-            <section className="fill-view scroll-snap-child background-navy">
-                
-            </section>
-            <section className="fill-view scroll-snap-child">
-                
-            </section>
-            <section className="fill-view scroll-snap-child background-green">
-                
-            </section>
-            <section className="fill-view scroll-snap-child">
-                
-            </section>
+            <div>
+                <section className="fill-view scroll-snap-child flex-center">
+
+                    <div className="x-center-text padding-l">
+                        <TypingTitle titles={props.titles}
+                            titleHangTime="4000"
+                            emptyHangTime="1700" />
+                    </div>
+                </section>
+                <section className="fill-view scroll-snap-child background-navy">
+                    
+                </section>
+                <section className="fill-view scroll-snap-child">
+                    
+                </section>
+                <section className="fill-view scroll-snap-child background-green">
+                    
+                </section>
+                <section className="fill-view scroll-snap-child">
+                    
+                </section>
+            </div>
+            <div className="scroll-arrow ">
+                <FontAwesomeIcon icon={faAngleDown} />
+            </div>
         </div>
     );
 }
