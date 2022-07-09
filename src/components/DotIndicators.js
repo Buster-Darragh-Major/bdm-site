b97f762b7dba8d15e18dfec12a0d41d5a7718c8d
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2022-07-09 13:32:55 +0200

    add on click smooth scrolling for dot indicators

--- src/components/DotIndicators.js
+++ src/components/DotIndicators.js
@@ -1,12 +1,25 @@
 import "./DotIndicators.scss";
 import useScrollVisible from "../hooks/useScrollVisible";
 
+
 function DotIndicators(props) {
     const pos = useScrollVisible(props.elementsContainerId, props.threshold);
+    
+    function dot_onClick(e) {
+        e.preventDefault();
+        const index = e.target.dataset.index;
+        const element = document.getElementById(props.elementsContainerId).children[index];
+        element.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
 
     var dots = [];
     for (var i = 0; i < props.count || 0; i++) {
-        dots.push(<div key={i} className={`dot-indicator ${pos === i ? "dot-indicator--selected" : ""}`}></div>);
+        dots.push(
+            <div key={i} 
+                data-index={i}
+                className={`dot-indicator ${pos === i ? "dot-indicator--selected" : ""}`}
+                onClick={dot_onClick}></div>
+        );
     }
 
     return (
