3e436d41d473b6078805dc511c747f4d72129347
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2022-07-12 22:03:59 +0200

    wip links page

--- src/components/IconLink.js
+++ src/components/IconLink.js
@@ -4,9 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 function IconLink(props) {
     return (
         <a className="icon-link-container" href={props.url} rel="noopener noreferrer" target="_blank">
-            <div className="icon-link-wipe"></div>
             <FontAwesomeIcon icon={['fab', props.icon]} />
-            <span>{props.name}</span>
+            <span>{props.displayName}</span>
         </a>
     );
 }
