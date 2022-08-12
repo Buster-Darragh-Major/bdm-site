20744abeae845e5b987d6d0832ec2a9a472194bc
busterdarragh-major <busterjdarraghmajor@gmail.com>
2022-08-12 15:19:40 +0100

    render recent songs

--- src/components/LastFm.js
+++ src/components/LastFm.js
@@ -5,19 +5,39 @@ function LastFm(props) {
     let defaultObject = {
         topAlbums: [],
         userInfo: {},
-        recentSongs: []
+        recentTracks: []
     };
     const [state, setState] = useState(defaultObject);
 
     if (state.topAlbums.length === 0) {
         GetAlbums().then(albums => {
-            setState(state => ({ ...state, topAlbums: albums.topalbums.album}));
+            setState(state => ({ ...state, topAlbums: albums.topalbums.album }));
+        });
+    }
+    if (state.recentTracks.length === 0) {
+        GetRecentTracks().then(tracks => {
+            setState(state => ({ ...state, recentTracks: tracks.recenttracks.track }));
         });
     }
 
+    return (
+        <div className="lastfm-container">
+            <div className="lastfm-title">What I've been listening to</div>
+            <div className="grid-tiles-container">
+                { RenderAlbums(state.topAlbums) }
+            </div>
+            <div className="lastfm-currently-listening">
+                { RenderRecentTracks(state.recentTracks) }
+            </div>
+        </div>
+    );
+}
+
+// private functions
+
+function RenderAlbums(topAlbums) {
     let albums = [];
-    state.topAlbums.forEach((topAlbum, i) => {
-        console.log(topAlbum);
+    topAlbums.forEach((topAlbum, i) => {
         albums.push(
             <div className="grid-tile" key={i}>
                 <a href={topAlbum.url} rel="noopener noreferrer" target="_blank">
@@ -35,17 +55,50 @@ function LastFm(props) {
         )
     });
 
-    return (
-        <div className="grid-tiles-container">
-            { albums }
-        </div>
-    );
+    return albums;
 }
 
-// private functions
+function RenderRecentTracks(recentTracks) {
+    let tracks = [];
+
+    recentTracks.forEach((track, i) => {
+        //debugger;
+        tracks.push(<div key={i}>
+            <div>{track.name}</div>
+            <div>{track.artist["#text"]}</div>
+            <div>{track.album["#text"]}</div>
+            <div>{TimestampToHumanReadable(track.date.uts)}</div>
+            <img src={track.image.find(img => img.size === "medium")["#text"]} alt={`${track.artist.name} - ${track.name}`}></img>
+        </div>);
+    });
+
+    return tracks;
+}
+
+function TimestampToHumanReadable(timeStamp) {
+    const nowSecs = Math.floor(Date.now() / 1000);
+    const diffSecs = nowSecs - timeStamp;
+
+    let representations = [
+        { label: "year", value: Math.floor(diffSecs / 31536000) },
+        { label: "month", value: Math.floor(diffSecs / 2628288) },
+        { label: "week", value: Math.floor(diffSecs / 604800) },
+        { label: "day", value: Math.floor(diffSecs / 86400) },
+        { label: "hour", value: Math.floor(diffSecs / 3600) },
+        { label: "minute", value: Math.floor(diffSecs / 60) },
+        { label: "second", value: diffSecs }
+    ];
+
+    let renderedRep = representations.find(rep => rep.value > 0);
+    return `${renderedRep.value} ${renderedRep.label}${renderedRep.value > 1 ? "s" : ""} ago`;
+}
 
 function GetAlbums() {
     return fetchJson("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=repoman63&api_key=d22b78878dae20222165cbca4b0ef8d2&format=json&period=1month&limit=24");
+}
+
+function GetRecentTracks() {
+    return fetchJson("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=repoman63&api_key=d22b78878dae20222165cbca4b0ef8d2&format=json&limit=3");
 }
 
 function fetchJson(url) {
