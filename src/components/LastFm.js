634045a99ad1c6c68b5456990c32b97a0cbdfdf4
busterdarragh-major <busterjdarraghmajor@gmail.com>
2022-08-20 17:11:47 +0100

    (WIP) user info query

--- src/components/LastFm.js
+++ src/components/LastFm.js
@@ -19,6 +19,11 @@ function LastFm(props) {
             setState(state => ({ ...state, recentTracks: tracks.recenttracks.track }));
         });
     }
+    if (Object.keys(state.userInfo).length === 0) {
+        GetUserInfo().then(userInfo => {
+            setState(state => ({ ...state, userInfo: userInfo }));
+        });
+    }
 
     return (
         <div className="lastfm-container">
@@ -28,6 +33,9 @@ function LastFm(props) {
             </div>
             <div className="lastfm-currently-listening">
                 { RenderRecentTracks(state.recentTracks) }
+            </div>
+            <div className="lastfm-user-info">
+                {JSON.stringify(state.userInfo)}
             </div>
         </div>
     );
@@ -64,12 +72,12 @@ function RenderRecentTracks(recentTracks) {
     recentTracks.forEach((track, i) => {
         let nowPlaying = track["@attr"]?.nowplaying;
         tracks.push(
-            <a key={i} className="lastfm-song" href={track.url} rel="noopener noreferrer" target="_blank">
+            <a key={i} className="lastfm-song link-remove-underline" href={track.url} rel="noopener noreferrer" target="_blank">
                 <div className="lastfm-img-container">
                     <img src={track.image.find(img => img.size === "medium")["#text"]} alt={`${track.artist.name} - ${track.name}`}></img>
                 </div>
                 <div className="lastfm-song-info">
-                    <div className="lastfm-song-name">{track.name}rg rfr gfd g frhgfrhger hgrehg trh ni</div>
+                    <div className="lastfm-song-name">{track.name}</div>
                     <div className="lastfm-artist-name">{track.artist["#text"]}</div>
                     <div className="lastfm-album-name">{track.album["#text"]}</div>
                     <div className="lastfm-time-played">{nowPlaying ? "Now Playing!" : TimestampToHumanReadable(track.date?.uts)}</div>
@@ -99,11 +107,15 @@ function TimestampToHumanReadable(timeStamp) {
 }
 
 function GetAlbums() {
-    return fetchJson("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=repoman63&api_key=d22b78878dae20222165cbca4b0ef8d2&format=json&period=1month&limit=24");
+    return fetchJson("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=repoman63&api_key=d22b78878dae20222165cbca4b0ef8d2&format=json&period=1month&limit=12");
 }
 
 function GetRecentTracks() {
     return fetchJson("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=repoman63&api_key=d22b78878dae20222165cbca4b0ef8d2&format=json&limit=3");
+}
+
+function GetUserInfo() {
+    return fetchJson("http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=repoman63&api_key=d22b78878dae20222165cbca4b0ef8d2&format=json");
 }
 
 function fetchJson(url) {
